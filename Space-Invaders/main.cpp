1179c78086937a74c353446372e12430fc4083f7
ImJarvis <its.me.siddh@gmail.com>
2024-06-20 00:06:00 +0530

    Dealing with sprite an texture

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -35,6 +35,24 @@ int main()
         rect.setPosition(800, 600-i);
         window.draw(rect);
         i += 1;
+
+
+        sf::Texture outscal_texture;
+        outscal_texture.loadFromFile("assets/textures/outscal_logo.png");
+
+        sf::Sprite outscal_sprite;
+        outscal_sprite.setTexture(outscal_texture);
+
+        outscal_sprite.setPosition(350, 400); // Position
+        outscal_sprite.setRotation(45+i); // Rotation in degrees
+        outscal_sprite.setScale(0.5, 0.5); // Scale factor
+
+        window.draw(outscal_sprite);
+
+
+
+
+
         // Display whatever you draw
         window.display();
     }
